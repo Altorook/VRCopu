e4565c9e95012472ae94d9000ff24894292f0490
altorook0542 <nathanberry403@gmail.com>
2025-08-10 21:31:01 -0600

    Game system progress

--- Assets/GameSystem.cs
+++ Assets/GameSystem.cs
@@ -1,16 +1,81 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameSystem : MonoBehaviour
 {
+    public enum GameState
+    {
+        DontPoint=0,
+        Chambering=1,
+        Shooting=2,
+        Checkclear=3,
+        Jam=4,
+        FreeTime=5
+    }
+    public GameState state = GameState.DontPoint;
+
+    [SerializeField] 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
+    public void DontPoint()
+    {
 
+    }
+    public void Chamber()
+    {
+
+    }
+    public void Shooting()
+    {
+
+    }
+    public void CheckClear()
+    {
+
+    }
+    public void Jam()
+    {
+
+    }
+    public void FreeTime()
+    {
+
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        switch (state)
+        {
+            case GameState.DontPoint:
+                DontPoint();
+                break;
+
+            case GameState.Chambering:
+                Chamber();
+                break;
+
+            case GameState.Shooting:
+                Shooting();
+                break;
+
+            case GameState.Checkclear:
+                CheckClear();
+                break;
+
+            case GameState.Jam:
+                Jam();
+                break;
+
+            case GameState.FreeTime:
+                FreeTime();
+                break;
+
+            default:
+                Debug.LogWarning("Unknown game state");
+                break;
+        }
     }
 }
